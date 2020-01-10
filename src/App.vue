1f5eb6072ed4e3786d0ec5953809f584f1f4b0c1
Shankar <psgganesh@gmail.com>
2020-01-11 01:04:29 +0800

    With vuetify

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height">
+        <v-layout wrap class="justify-center align-center">
+          <v-flex xs12>
+            <router-view />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+.fill-height {
+  height: 100vh;
+}
+</style>
