96843433456a6abf94f7988d1539dffeb8971c45
Shankar <psgganesh@gmail.com>
2020-01-10 17:54:51 +0800

    Raw version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import objectDetectionSketch from './ObjectDetectionSketch';
+
+import P5Wrapper from 'react-p5-wrapper';
+
+function App() {
+  return (
+    <P5Wrapper sketch={objectDetectionSketch} />
+  );
+}
+
+export default App;
